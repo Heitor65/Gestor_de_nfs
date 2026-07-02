2ed594a5c40b04a9da888608fb52721f0998104e
Heitor65 <multheitor@gmail.com>
2026-07-01 21:22:14 -0300

    classe principal

--- nf_manager/models.py
+++ nf_manager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class nf(models.Model):
+    nome = models.CharField(max_length=100)
+    servico = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=[('pendente', 'Pendente'), ('aprovada', 'Aprovada'), ('rejeitada', 'Rejeitada')], default='pendente')
+    descricao = models.TextField()
