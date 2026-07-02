b7305468a23dad2598c3c3729eba87defd9ceb20
Heitor65 <multheitor@gmail.com>
2026-07-01 22:41:43 -0300

    personalizando admin

--- nf_manager/admin.py
+++ nf_manager/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import nf
+from .models import Nf
 
-
-admin.site.register(nf)
+@admin.register(Nf)
+class NfAdmin(admin.ModelAdmin):
+    list_display = ('razao_social', 'cnpj', 'op', 'pessoa_contato', 'servico', 'status')
+    search_fields = ('razao_social', 'cnpj', 'op', 'pessoa_contato')
+    fieldsets = (
+        (None, {
+            'fields': ('razao_social', 'cnpj', 'op', 'inscricao_estadual', 'inscricao_municipal')
+        }),
+        ('Endereço', {
+            'fields': ('cep', 'uf', 'cidade', 'bairro', 'logradouro', 'numero', 'complemento')
+        }),
+        ('Informações de Contato', {
+            'fields': ('pessoa_contato', 'numero_contato', 'email_contato')
+        }),
+        ('Serviço e Status', {
+            'fields': ('servico', 'valor', 'status')
+        }),
+        (None, {
+            'fields': ('descricao',)
+        }),
+    )
+    list_filter = ('status',)
