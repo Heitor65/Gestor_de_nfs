f51cbfa757596d1f0c954a58bf84f80951074219
Heitor65 <multheitor@gmail.com>
2026-07-01 22:41:25 -0300

    atributos reais da classe

--- nf_manager/models.py
+++ nf_manager/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
-class nf(models.Model):
-    nome = models.CharField(max_length=100)
-    servico = models.CharField(max_length=100)
+class Nf(models.Model):
+    razao_social = models.CharField(max_length=100, default="")
+    cnpj = models.CharField(max_length=14, default="")
+    op = models.CharField(max_length=4, default="")
+    cep = models.CharField(max_length=8, default="")
+    uf = models.CharField(max_length=2, default="RJ")
+    cidade = models.CharField(max_length=100, default="")
+    bairro = models.CharField(max_length=100, default="")
+    logradouro = models.CharField(max_length=100, default="")
+    numero = models.CharField(max_length=10, default="")
+    complemento = models.CharField(max_length=100, default="")
+    inscricao_estadual = models.CharField(max_length=20, default="")
+    inscricao_municipal = models.CharField(max_length=20, default="")
+    pessoa_contato = models.CharField(max_length=100, blank=True, null=True)
+    numero_contato = models.CharField(max_length=20, blank=True, null=True)
+    email_contato = models.EmailField(blank=True, null=True)
+    servico = models.CharField(max_length=100, default="")
     valor = models.DecimalField(max_digits=8, decimal_places=2)
     status = models.CharField(max_length=20, choices=[('pendente', 'Pendente'), ('aprovada', 'Aprovada'), ('rejeitada', 'Rejeitada')], default='pendente')
     descricao = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"NF {self.razao_social} - {self.cnpj} - {self.op}"
