25b1ab1bcd6d9957f5ec8e0bedbe7e8de5083db7
Heitor65 <multheitor@gmail.com>
2026-07-01 22:08:09 -0300

    melhorando classe nf

--- nf_manager/models.py
+++ nf_manager/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class nf(models.Model):
     nome = models.CharField(max_length=100)
     servico = models.CharField(max_length=100)
-    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    valor = models.DecimalField(max_digits=8, decimal_places=2)
     status = models.CharField(max_length=20, choices=[('pendente', 'Pendente'), ('aprovada', 'Aprovada'), ('rejeitada', 'Rejeitada')], default='pendente')
-    descricao = models.TextField()
+    descricao = models.TextField(blanck=True, null=True)
