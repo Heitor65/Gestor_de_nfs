5bd411e112ebf80ad1813851bd6a23e8ce8341de
Heitor65 <multheitor@gmail.com>
2026-07-01 22:09:47 -0300

    fix: erro de digitação no models

--- nf_manager/models.py
+++ nf_manager/models.py
@@ -5,4 +5,4 @@ class nf(models.Model):
     servico = models.CharField(max_length=100)
     valor = models.DecimalField(max_digits=8, decimal_places=2)
     status = models.CharField(max_length=20, choices=[('pendente', 'Pendente'), ('aprovada', 'Aprovada'), ('rejeitada', 'Rejeitada')], default='pendente')
-    descricao = models.TextField(blanck=True, null=True)
+    descricao = models.TextField(blank=True, null=True)
