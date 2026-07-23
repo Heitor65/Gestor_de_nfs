a8e61d6b2e9f8c2dde902e980af66da5388656fd
Heitor65 <multheitor@gmail.com>
2026-07-22 22:15:08 -0300

    feat: permitindo adicionar campos com simbolos

--- nf_manager/models.py
+++ nf_manager/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.core.exceptions import ValidationError
-from brutils import is_valid_cnpj, is_valid_cep, get_address_from_cep,  is_valid_phone, is_valid_email
+from brutils import is_valid_cnpj,remove_symbols_cnpj, is_valid_cep, remove_symbols_cep, get_address_from_cep,  is_valid_phone, remove_symbols_phone, is_valid_email
 
 def _validate_cnpj(value):
-    if not is_valid_cnpj(value):
+    if not is_valid_cnpj(remove_symbols_cnpj(value)):
         raise ValidationError("%(value)s não é um CNPJ válido.", params={"value": value})
 
 def _validate_cep(value):
-    if value and not is_valid_cep(value):
+    if value and not is_valid_cep(remove_symbols_cep(value)):
         raise ValidationError("%(value)s não é um CEP válido.", params={"value": value})
 
 def _validate_phone(value):
-    if value and not is_valid_phone(value):
+    if value and not is_valid_phone(remove_symbols_phone(value)):
         raise ValidationError("%(value)s não é um telefone válido.", params={"value": value})
 
 def _validate_email(value):
